b16a241ce07ab3e85d79ea7fe828250010a622e8
Coarf <coarf@hotmail.com>
2022-02-24 16:12:21 -0500

    creacion del modulo auth y estilos

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+# from flask_wtf.csrf import CsrfProtect
 
 # local imports
 from config import app_config
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # run.py
 
 import os
+from flask_wtf.csrf import CsrfProtect
 
 from app import create_app
 
@@ -8,5 +9,6 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
+    CsrfProtect(app)
     app.run()
 
